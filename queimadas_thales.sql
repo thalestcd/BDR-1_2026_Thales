9ba374268e9829721108e1825c05d011a4483f8d
thalestcd <39998018+thalestcd@users.noreply.github.com>
2026-02-19 16:51:00 -0300

    Update queimadas_thales.sql

--- queimadas_thales.sql
+++ queimadas_thales.sql
@@ -1,1 +1,16 @@
-SELECT * FROM focos_calor;
+--Thales Cambraia Dias Atividade 1 - BDR-1-2026--
+
+CREATE TABLE focos_calor (
+		id SERIAL PRIMARY KEY,
+		estado VARCHAR(50),
+		bioma VARCHAR(50),
+		data_ocorrencia DATE
+        );
+
+INSERT INTO focos_calor(estado,bioma,data_ocorrencia) VALUES
+		('Amazonas','Amazônia','2025-02-01'),
+		('Mato Grosso','Cerado','2025-02-03'),
+		('Pará','Amazônia','2025-02-05');
+
+	
+Select * from focos_calor
